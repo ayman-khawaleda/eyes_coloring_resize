8dcb1c0bcb48829b76aa14e4763c4c4c46eda16a
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-05-29 01:42:10 +0300

    Init Repo & Add Resources & .gitignore

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+IMAGE_FILES = [
+    r"Resources/man1.jpg",
+    r"Resources/man2.jpg",
+    r"Resources/woman1.jpg",
+    r"Resources/woman2.jpg",
+]
+
+if __name__ == "__main__":
+    print("Hello, World")
