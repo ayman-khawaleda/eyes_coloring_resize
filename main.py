c791ed9233c7830f1c18e2adb1846efaca978da0
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-05-30 01:25:39 +0300

    Fix Main - Missed To Close The FaceMesh Parenthesis

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
         max_num_faces=1,
         refine_landmarks=True,
         min_detection_confidence=0.5,
-    for i,path in enumerate(IMAGE_FILES):
-        cet = CET(path,face_mesh)
-        cet.apply(color=(120,60),saturation=25)
+    )
+    for i, path in enumerate(IMAGE_FILES):
+        cet = CET(path, face_mesh)
+        cet.apply(color=(120, 60), saturation=25)
         cet.show_results()
-
