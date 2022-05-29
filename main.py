2c97d5254ce49f5a19e5d0bba56c496a9fa8a99b
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-05-29 22:32:19 +0300

    Update Main to work with Iris coloing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import mediapipe as mp
 
 IMAGE_FILES = [
     r"Resources/man1.jpg",
@@ -8,4 +8,15 @@ IMAGE_FILES = [
 ]
 
 if __name__ == "__main__":
-    print("Hello, World")
+    mp_face_mesh = mp.solutions.face_mesh
+    mp_face_detecation = mp.solutions.face_detection
+
+    face_detecation = mp_face_detecation.FaceDetection(
+        model_selection=1, min_detection_confidence=0.5
+    )
+    face_mesh = mp_face_mesh.FaceMesh(
+        static_image_mode=True,
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5,
+    )
