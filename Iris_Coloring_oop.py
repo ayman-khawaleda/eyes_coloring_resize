f3d55d2ac442c7c4f2b398c2a0a6c03b9d923671
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-05-29 22:45:05 +0300

    Checkout Whether Eyes are Open or Not

--- Iris_Coloring_oop.py
+++ Iris_Coloring_oop.py
@@ -30,4 +30,36 @@ class ColoringEyeTool(EyeTool):
         if not results.multi_face_landmarks:
             raise Exception(f'No Faces Detected In Image With Path: "{self.path}".')
 
+    def __is_eyes_open(self, face_landmarks, dist: int = 15):
+        right_min_p, right_max_p, left_min_p, left_max_p = (
+            decimal.MAX_EMAX,
+            0,
+            decimal.MAX_EMAX,
+            0,
+        )
+        mp_face_mesh = mp.solutions.face_mesh
+        h, w, _ = self.image.shape
+        for tup1, tup2 in zip(
+            mp_face_mesh.FACEMESH_RIGHT_EYE, mp_face_mesh.FACEMESH_LEFT_EYE
+        ):
+            # Finding Both minimum & maximum values of right eyelid
+            sor_idx, _ = tup1
+            source = face_landmarks.landmark[sor_idx]
+            norm = self.normaliz_pixel(source.x, source.y, w, h)
+            if norm[1] > right_max_p:
+                right_max_p = norm[1]
+            if norm[1] < right_min_p:
+                right_min_p = norm[1]
+
+            # Finding Both minimum & maximum values of left eyelid
+            sor_idx, _ = tup2
+            source = face_landmarks.landmark[sor_idx]
+            norm = self.normaliz_pixel(source.x, source.y, w, h)
+            if norm[1] > left_max_p:
+                left_max_p = norm[1]
+            if norm[1] < left_min_p:
+                left_min_p = norm[1]
+
+        # Calculating the Distance between the two values
+        return (right_max_p - right_min_p > dist, left_max_p - left_min_p > dist)
 
